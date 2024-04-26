36618fcd5312d04d4d22d13ee8d7781e3486d000
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-04-26 15:12:43 +0530

    Add ClerkProvider to layout.tsx and clerkMiddleware to middleware.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ConvexClientProvider from "./ConvexClientProvider";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
