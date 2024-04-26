74a28e123bed9e25a2644336f50075413d55cc2b
Patil Kaustubh <patilkaustubh990@gmail.com>
2024-04-26 14:58:13 +0530

    next + convex setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import ConvexClientProvider from "./ConvexClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Fileify Storage",
   description: "A multi-file storage application like google drive, which works on role based model",
+  
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
