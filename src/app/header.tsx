fe24712f4101d37eeb11e06923cbb08329657c2d
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-07-08 20:44:29 +0530

    refactor: Update header component and add ClerkProvider to layout

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { UserButton, UserProfile } from "@clerk/nextjs";
 
-export const header = () => {
-  return (
-    <div>header</div>
-  )
+export function Header() {
+    return  <div className="border-b y-4 bg-50">
+        <div className="container mx-auto justify-between flex">
+            This is a header
+            <UserButton />
+        </div>
+
+    </div>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import ConvexClientProvider from "./ConvexClientProvider";
+import { Header } from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
     <body className={inter.className}>
       <ConvexClientProvider>
+        <Header />
         {children}
       </ConvexClientProvider>
     </body>
